6a1ce4db1771cf2af23839a8910a2b250dd0f6b2
Dhruthi NOOKA <DNO25740@open-groupe.com>
2025-10-08 11:48:46 +0200

    Event Modifiers

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,24 @@
 <template>
-  <h1 @click="userClick">Hello World</h1>
-  <input @input="userInput($event, 'world')" type="text" />
+  <div @click="userClick('div')">
+    <h1 @click.stop.left="userClick('h1')">Hello World</h1>
+  </div>
+  <input @keyup.enter="mykeyup">
+  <form @submit.prevent="formSubmit">
+    <button>Submit</button>
+  </form>
+
 </template>
 <script setup lang="ts">
-function userClick(event: MouseEvent) {
-  console.log(event)
+function userClick(from: string) {
+  console.log('click from ', from);
 }
 
-function userInput(event: Event, str: string) {
-  console.log(str)
+function formSubmit() {
+  console.log('form submitted');
+}
+
+function mykeyup() {
+  console.log('mykeyup');
 }
 </script>
 <style></style>
