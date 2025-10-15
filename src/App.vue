5766595c47220d417c023d30f205485828f3e040
Dhruthi NOOKA <DNO25740@open-groupe.com>
2025-10-15 08:44:58 +0200

    Use of sass

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,14 @@
 <template>
-  <h1
-    :style="{
-      'font-size': fontSize,
-      color: color,
-    }"
-  >
-    Bonjour le monde !
-  </h1>
-  <h2
-    :style="h1style"
-  >
-    Bienvenue !
-  </h2>
-  <h3 :style="{
-      fontSize: btnFontSize + 'px'
-  }">
-    Boom
-  </h3>
-  <button @click="btnFontSize += 10">+</button>
-  <button @click="btnFontSize -= 10">-</button>
-  <h4 :style="[fontSizeStr, colorStr]">Hello</h4>
+  <h1><span>Hi!</span> Hello</h1>
 </template>
-<!--un tableau de diff styles-->
 <script setup lang="ts">
-import { reactive, ref } from 'vue'
-
-// to declare individual reactive props
-const fontSize = ref('5px')
-const color = ref('red')
-
-// to declare an obj with multiple props as reactive
-const h1style = reactive({
-  fontSize: '20px',
-  color: 'blue'
-})
-
-const btnFontSize = ref(10)
-
-const fontSizeStr = ref('font-size:15px;')
-const colorStr = ref('color:green;')
 
 </script>
-<style scoped></style>
-<script setup lang="ts">
-</script>
+<style scoped lang="scss">
+  h1 {
+    color: green;
+    span {
+      color: blue;
+    }
+  }
+</style>
