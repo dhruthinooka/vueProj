9758914e1129eda6b9a55139a5fcf587a082e714
Dhruthi NOOKA <DNO25740@open-groupe.com>
2025-10-15 16:01:41 +0200

    v-show, v-pre Directives

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,13 @@
 <template>
-
-  <div>
-    <h1>Profil</h1>
-    <ul v-if="state.user">
-      <li>Name: {{ state.user?.name }}</li>
-      <li>Age: {{ state.user?.age }}</li>
-    </ul>
-    <h2 v-else>Chargement ...</h2>
-    <input v-model="letter" type="text" />
-    <h1 v-if="letter === 'A'">A</h1>
-    <h1 v-else-if="letter === 'B'">B</h1>
-    <h1 v-else-if="letter === 'C'">C</h1>
-    <h1 v-else>Autre</h1>
-  </div>
-
+  <button @click="toggle = !toggle">Toggle</button>
+  <h1 v-show="toggle">Bonjour le monde!</h1>
+  <h2 v-pre> {{ toggle }}</h2>
 </template>
 <script setup lang="ts">
-import { reactive, ref } from 'vue'
+import { ref } from 'vue'
 
-const state = reactive<any>({});
-
-setTimeout(() => {
-  state.user = {
-    name: 'David',
-    age: 5,
-  }
-}, 3000)
-
-const letter = ref('')
+const toggle = ref(false);
 
 </script>
 <style scoped lang="scss">
-  h1 {
-    color: green;
-    span {
-      color: blue;
-    }
-  }
 </style>
