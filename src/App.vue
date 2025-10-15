128789a68341bcd1eb41ffb788367ade20c29197
Dhruthi NOOKA <DNO25740@open-groupe.com>
2025-10-15 07:50:07 +0200

    class binding

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,51 @@
 <template>
-  <input type="text" v-model="text" />
-  <h1>{{ text }}</h1>
-  <input type="number" v-model="product.quantity" />
-  <input type="number" v-model="price" />
-  <h1>Prix total HT: {{ totalPriceHT }}</h1>
-  <h2>Prix total TTC: {{ totalPriceTTC }}</h2>
+  <input
+    class="input"
+    :class="{
+      inputOngoing,
+      inputError,
+      inputValid
+    }"
+    @focus="
+      input.focus = true;
+      input.touched = true
+    "
+    @blur="input.focus = false"
+    type="text"
+    v-model="input.value"
+  />
 </template>
 <script setup lang="ts">
-import { computed, reactive, ref, watch, watchEffect } from 'vue'
+import { computed, reactive } from 'vue'
 
-interface Product {
-  name: string,
-  quantity: number,
-  priceHT: number,
-  nbrOfModification: number,
-  lastModification: number | null
-}
-const text = ref('')
-
-const product = reactive<Product>({
-  name: 'books',
-  quantity: 1,
-  priceHT: 10,
-  nbrOfModification: 0,
-  lastModification:  null
+const input = reactive({
+  value: '',
+  focus: false,
+  touched: false
 })
-
-const price = ref(0);
-const totalPriceHT = computed(() => {
-  return product.priceHT * product.quantity
-})
-const totalPriceTTC = computed(() => {
-  return product.priceHT * product.quantity * 1.2
-})
-
-watch([() => product.quantity, price], (newValue, oldValue) => {
-  product.nbrOfModification++;
-  console.log(newValue);
-  console.log(oldValue);
-})
-
-watchEffect(() => {
-  console.log('in watchEffect');
-  product.priceHT = price.value;
-  product.lastModification = Date.now()
-  console.log(product);
-})
+const inputOngoing = computed(() => input.focus
+  && input.value.length)
+const inputError = computed(() => !input.focus && input.touched
+  && input.value.length <= 5)
+const inputValid = computed(() => input.touched
+  && !input.focus && !inputError.value)
 </script>
-<style></style>
+<style scoped>
+.input {
+  outline: 0;
+  border: 2px solid black;
+  border-radius: 4px;
+}
+
+.inputOngoing {
+  border-color: cadetblue;
+}
+
+.inputError {
+  border-color: red;
+}
+
+.inputValid {
+  border-color: green;
+}
+</style>
