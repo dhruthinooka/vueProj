b958a80a6759055f3ddefd2d2b02a5a51834094f
Dhruthi NOOKA <DNO25740@open-groupe.com>
2025-10-15 15:48:16 +0200

    v-if, v-else, v-else-if Directives

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
 <template>
-  <h1><span>Hi!</span> Hello</h1>
+
+  <div>
+    <h1>Profil</h1>
+    <ul v-if="state.user">
+      <li>Name: {{ state.user?.name }}</li>
+      <li>Age: {{ state.user?.age }}</li>
+    </ul>
+    <h2 v-else>Chargement ...</h2>
+    <input v-model="letter" type="text" />
+    <h1 v-if="letter === 'A'">A</h1>
+    <h1 v-else-if="letter === 'B'">B</h1>
+    <h1 v-else-if="letter === 'C'">C</h1>
+    <h1 v-else>Autre</h1>
+  </div>
+
 </template>
 <script setup lang="ts">
+import { reactive, ref } from 'vue'
+
+const state = reactive<any>({});
+
+setTimeout(() => {
+  state.user = {
+    name: 'David',
+    age: 5,
+  }
+}, 3000)
+
+const letter = ref('')
 
 </script>
 <style scoped lang="scss">
